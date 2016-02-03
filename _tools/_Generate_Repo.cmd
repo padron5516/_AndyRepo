528e0ad43c8b6e821a13e29e8542456b8352757c
halikus <optimuscrim@gmail.com>
2016-02-02 21:39:32 -0500

    Failsafe Modules

--- _tools/_Generate_Repo.cmd
+++ _tools/_Generate_Repo.cmd
@@ -5,4 +5,4 @@ for /R _repo %%f in (*.zip) do del /q "%%~ff"
 cd _tools
 C:\Python27\python.exe generate_repo.py
 cd..
-echo F | xcopy "_repo\Andy.repository\Andy.repository-1.0.zip" "F:\_Github\_AndyRepo\Andy.repository.zip" /cherkyi
+echo F | xcopy "_repo\Andy.repository\Andy.repository-1.0.0.zip" "F:\_Github\_AndyRepo\Andy.repository.zip" /cherkyi
