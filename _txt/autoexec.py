45fa705c65063152c7c31dbbd9d2aca9e8da4e5e
halikus <optimuscrim@gmail.com>
2016-03-23 15:07:19 -0400

    repo

--- _txt/autoexec.py
+++ _txt/autoexec.py
@@ -3,3 +3,14 @@ import xbmc
 #xbmc.executebuiltin('XBMC.RunScript(script.audio.spotimc)')
 #xbmc.executebuiltin('XBMC.ActivateWindow(myvideolibrary,movietitles)')
 #xbmc.executebuiltin('XBMC.ActivateWindow(videofiles,x:\downloads\)')
+
+
+# Clean cache
+import os
+import xbmc
+path = xbmc.translatePath( "special://temp" )
+filenames = next(os.walk(path))[2]
+for i in filenames:
+    if ".fi" in i:
+        os.remove(os.path.join(path, i))
+
